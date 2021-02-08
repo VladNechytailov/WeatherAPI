b639cb5c7e78dbe3bc79ce6c099c67ad69f9975c
Vlad Nechytailov <vlad.nechytailov@gmail.com>
2021-02-08 13:06:04 +0200

    city parameter was changed to required and answer format was changed to json

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -3,6 +3,8 @@ using System.Net.Http;
 using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Collections.Generic;
 
 namespace WeatherAPI.Controllers
 {
@@ -19,48 +21,48 @@ namespace WeatherAPI.Controllers
             HttpResponseMessage response = await client.GetAsync(url);
             JObject answer = JObject.Parse(await response.Content.ReadAsStringAsync());
             if((int)answer["cod"] != 200)
-                throw new Exception("Response code: " + (string)answer["cod"] + "\nMessage: " + (string)answer["message"] + ".");
+                throw new Exception(answer.ToString());
             return answer;
         }
 
-        [HttpGet("GetCurrentWeather")]
+        [HttpGet("GetCurrentWeather{city}")]
         public async Task<string> GetCurrentWeather(string city)
         {
             string url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + key + "&units=" + units;
             try
             {
                 JObject answer = await GetAnswer(url);
-                return "Погода в данный момент\n" + new CurrentWeather
+                return JsonSerializer.Serialize(new CurrentWeather
                 {
                     Date = new DateTime(1970, 1, 1).Add(TimeSpan.FromTicks((int)answer["dt"] * TimeSpan.TicksPerSecond)).ToLocalTime(),
                     Temperature = (int)answer["main"]["temp"],
                     WindSpeed = (float)answer["wind"]["speed"],
                     Cloudiness = (int)answer["clouds"]["all"],
-                }.ToString();
+                });
             }
             catch(Exception e) {
                 return e.Message;
             }            
         }
 
-        [HttpGet("GetWeatherForecast")]
+        [HttpGet("GetWeatherForecast{city}")]
         public async Task<string> GetWeatherForecast(string city)
         {
             string url = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=" + key + "&units=" + units;
             try
             {
                 JObject answer = await GetAnswer(url);
-                string result = "Прогноз погоды на ближайшие 5 дней";
+                List<WeatherForecast> result = new List<WeatherForecast>();
                 foreach(var i in answer["list"])
-                    result += "\n" + new WeatherForecast
+                    result.Add(new WeatherForecast
                     {
                         Date = DateTime.Parse((string)i["dt_txt"]),
                         MaxTemperature = (int)i["main"]["temp_max"],
                         MinTemperature = (int)i["main"]["temp_min"],
                         WindSpeed = (float)i["wind"]["speed"],
                         Cloudiness = (int)i["clouds"]["all"],
-                    }.ToString();
-                return result;
+                    });
+                return JsonSerializer.Serialize(result);
             }
             catch(Exception e) {
                 return e.Message;
